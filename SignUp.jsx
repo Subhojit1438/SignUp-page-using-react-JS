bbc4a91922d24a4f3045b5f43fecd6dcdf7127ea
Subhojit Roy <139738518+Subhojit1438@users.noreply.github.com>
2024-01-09 18:07:21 +0530

    Update SignUp.jsx

--- SignUp.jsx
+++ SignUp.jsx
@@ -1,38 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SignUp.module.css'
 import { BiSolidUserDetail } from "react-icons/bi";
 import { FaUserCircle } from "react-icons/fa";
-import { MdEmail,MdSubdirectoryArrowRight } from "react-icons/md";
+import { MdEmail, MdSubdirectoryArrowRight } from "react-icons/md";
 import { RiLockPasswordLine, RiLockPasswordFill } from "react-icons/ri";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate
+} from 'react-router-dom'
+import Nav from './Nav';
+import { Children } from 'react';
 
-export const SignUp = () => {
+export const SignUp = ({Children}) => {
+  const [action, setAction] = useState("Login")
+  const navigate = useNavigate();
+  const navigateToNav = () => {
+    navigate("/Nav")
+  }
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.background}>
-          <label className={styles.login}>Already have an account?</label>
+          <label className={styles.login}>Already have an account?<br /><button className={styles.login_a}>Login</button></label>
         </div>
-        <button className={styles.login_a}>Login</button>
         <form className={styles.loginform}>
-          <h1>SignUp</h1>
-          <input className={`${styles.inputbox} ${styles.effect_2}`} type="text" placeholder='Full Name' required />
-          <BiSolidUserDetail className={styles.icon} />
+          <h1>{action}</h1>
+          {
+            action === "Login" ? <div></div> : <div><input className={styles.inputbox} type="text" placeholder='Full Name' required />
+              <BiSolidUserDetail className={styles.icon} /></div>
+          }
           <input className={styles.inputbox} type="text" placeholder='Username' required />
           <FaUserCircle className={styles.icon} />
-          <input className={styles.inputbox} type="text" placeholder='Email' required />
-          <MdEmail className={styles.icon} />
+          {
+            action === "Login" ? <div></div> : <div>
+              <input className={styles.inputbox} type="text" placeholder='Email' required />
+              <MdEmail className={styles.icon} />
+            </div>
+          }
           <input className={styles.inputbox} type="password" placeholder='Password' required />
           <RiLockPasswordLine className={styles.icon} />
-          <input className={styles.inputbox} type="password" placeholder='Confirm Password' required />
-          <RiLockPasswordFill className={styles.icon} />
+          {
+            action === "Login" ? <div></div> : <div>
+              <input className={styles.inputbox} type="password" placeholder='Confirm Password' required />
+              <RiLockPasswordFill className={styles.icon} />
+            </div>
+          }
           <div className={styles.remember}>
             <input type="checkbox" id='check' />
             <label for='check'>Remember me</label>
-            <a href="#" className={styles.forgot}>Forgot password</a>
+            {
+              action === "Login" ? <div></div> : <div>
+                <a href="#" className={styles.forgot}>Forgot password</a>
+              </div>
+
+            }
           </div>
-          <button type="button" className={styles.signbutton}>SignUp</button>
+          <button type="button" className={styles.signbutton} onClick={navigateToNav}>{action}</button>
+          <Routes>
+            <Route path="/Nav" element={<Nav />}></Route>
+          </Routes>
         </form>
+        <main>{Children}</main>
       </div>
     </>
   )
+}
+
 }
